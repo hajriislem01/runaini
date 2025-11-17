62f3e670e9583cefd0c4d09654a57ec23d62b635
hajriislem01 <islem.hajri96@gmail.com>
2025-11-17 02:13:40 +0100

    ajout de postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'football_db',        
+        'USER': 'islem',         
+        'PASSWORD': 'admin',  
+        'HOST': 'localhost',          
+        'PORT': '5432',              
     }
 }
 
