ec3c5e137801a4c54187f71b606ef9d935cf3f6f
hajriislem01 <islem.hajri96@gmail.com>
2025-11-18 16:21:47 +0100

    add accounts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'rest_framework',
     'rest_framework.authtoken' ,
-    'accounts',
+    'backend.accounts',
     'django.contrib.staticfiles',  
     'corsheaders', 
     
