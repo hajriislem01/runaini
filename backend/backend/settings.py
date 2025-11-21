31d0c1eef02108044e64f3d1afe2165ec67e9274
hajriislem01 <islem.hajri96@gmail.com>
2025-11-21 13:37:40 +0100

    back to backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -72,7 +73,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:80"
 ]
 
-ROOT_URLCONF = 'backend.api.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -90,7 +91,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.api.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
