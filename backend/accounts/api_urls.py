d161004355fba3e7e328449dcabd5b0b4adb915b
hajriislem01 <islem.hajri96@gmail.com>
2025-11-23 03:00:47 +0100

    back to backend

--- backend/accounts/api_urls.py
+++ backend/accounts/api_urls.py
@@ -1,8 +1,8 @@
 # api_urls.py
 from rest_framework.routers import DefaultRouter
-from .views import CoachViewSet
+from .views import CoachViewSet , PlayerViewSet
 
 router = DefaultRouter()
 router.register(r'coaches', CoachViewSet, basename='CoachProfile')
-
+router.register(r'players', PlayerViewSet, basename='PlayerProfile')
 urlpatterns = router.urls
